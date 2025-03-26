abf650d4edefd00012be2d67c93e3877baa7824b
shunii <shunii85@outlook.jp>
2025-03-26 15:59:23 +0900

    feat: rewrite with header component

--- layouts/top-layout.tsx
+++ layouts/top-layout.tsx
@@ -1,27 +1,14 @@
-import { LinkButton } from "@/components/link-button"
+import { Header } from "@/components/layouts/header"
 import { FC, PropsWithChildren } from "react"
 
 export const TopLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="px-16">
-      <header className="fixed top-0 right-0 left-0 flex items-center justify-between bg-indigo-900 px-16 py-4 font-bold text-slate-300">
-        <h1 className="text-2xl tracking-tight">shunii .dev</h1>
-        <nav>
-          <ul className="hidden gap-1.5 sm:flex">
-            <li>
-              <LinkButton href="/">HOME</LinkButton>
-            </li>
-            <li>
-              <LinkButton href="/blog">BLOG</LinkButton>
-            </li>
-            <li>
-              <LinkButton href="/status">ABOUT</LinkButton>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main className="mt-20">{children}</main>
-      <footer>Copyright © 2024 Shuta Yanagawa</footer>
-    </div>
+    <>
+      <Header />
+      <main className="px-16">{children}</main>
+      <footer className="bg-indigo-900 p-8">
+        Copyright © 2024 Shuta Yanagawa
+      </footer>
+    </>
   )
 }
