016aea69a96b5cba59669a21b3b0f3b77c99fa29
shunii <shunii85@outlook.jp>
2025-03-31 21:34:59 +0900

    feat: made header controll to display by scroll

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -1,9 +1,19 @@
-import { FC, memo, useState } from "react"
+import {
+  ComponentPropsWithRef,
+  FC,
+  memo,
+  PropsWithRef,
+  Ref,
+  useEffect,
+  useRef,
+  useState
+} from "react"
 import Link from "next/link"
 import { Menu } from "lucide-react"
 import { LinkButton } from "@/components/link-button"
 import { Drawer } from "vaul"
 
+type Scroll = "up" | "down"
 const linkInfos = [
   { label: "HOME", href: "/" },
   { label: "BLOG", href: "/blog" },
@@ -11,27 +21,53 @@ const linkInfos = [
 ]
 
 export const Header: FC = memo(() => {
+  const headerRef = useRef<HTMLHeadingElement>(null)
   const [open, setOpen] = useState(false)
+  const [scroll, setScroll] = useState<Scroll>()
+  const [y, setY] = useState<number>(0)
+  const isDisplay = scroll === "up" || scroll === undefined
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const { scrollY } = window
+      setY(scrollY)
+      setScroll(() => {
+        if (scrollY < y) return "up"
+        return "down"
+      })
+    })
+    return () => {
+      window.removeEventListener("scroll", () => {
+        setY(window.scrollY)
+      })
+    }
+  })
+
   return (
     <>
-      <header className="sticky top-0 right-0 z-10 flex h-18 max-h-18 items-center justify-between bg-indigo-900 px-16 font-bold text-slate-300">
-        <h1 className="text-2xl tracking-tight">
-          <Link href="/">shunii .dev</Link>
-        </h1>
-        <nav>
-          <ul className="flex gap-1.5 text-sm">
-            {linkInfos.map(({ label, href }) => (
-              <li key={label} className="hidden sm:inline-flex">
-                <LinkButton className="hover:bg-indigo-950" href={href}>
-                  {label}
-                </LinkButton>
+      <header
+        ref={headerRef}
+        className={`sticky px-2 py-3 duration-700 ease-in-out sm:px-6 sm:py-5 ${isDisplay ? "translate-y-0" : "-translate-y-32"}`}
+      >
+        <div className="z-10 flex max-h-18 items-center justify-between rounded-full bg-indigo-900 px-10 py-2 font-bold text-slate-300 sm:px-16 sm:py-5">
+          <h1 className="text-xl tracking-tight sm:text-2xl">
+            <Link href="/">shunii .dev</Link>
+          </h1>
+          <nav>
+            <ul className="flex gap-1.5 text-sm">
+              {linkInfos.map(({ label, href }) => (
+                <li key={label} className="hidden sm:inline-flex">
+                  <LinkButton className="hover:bg-indigo-950" href={href}>
+                    {label}
+                  </LinkButton>
+                </li>
+              ))}
+              <li className="sm:hidden">
+                <MenuButton onClick={() => setOpen(true)} />
               </li>
-            ))}
-            <li className="sm:hidden">
-              <MenuButton onClick={() => setOpen(true)} />
-            </li>
-          </ul>
-        </nav>
+            </ul>
+          </nav>
+        </div>
       </header>
       <MobileMenu open={open} onClose={() => setOpen(false)} />
     </>
--- components/link-button.tsx
+++ components/link-button.tsx
@@ -13,7 +13,7 @@ export const LinkButton: FC<LinkButtonProps> = ({
 }) => {
   return (
     <Link href={href}>
-      <button className={`rounded-lg px-6 py-2 duration-500 ${className}`}>
+      <button className={`rounded-full px-6 py-2 duration-500 ${className}`}>
         {children}
       </button>
     </Link>
